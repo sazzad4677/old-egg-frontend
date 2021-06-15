9b93b492d789f0b22b76a4bbfb520ded06e4c05f
sazzad4677 <sazzad35-1898@diu.edu.bd>
2021-06-15 21:50:43 +0600

    total order price added

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -6,6 +6,7 @@ import Header from "../Header/Header";
 const Order = () => {
   const [orders, setOrders] = useState([]);
   const [loggedIn, setLoggedIn] = useContext(userContext);
+  // const [productPrice] = orders
   useEffect(() => {
     fetch("https://glacial-mesa-63021.herokuapp.com/orders?email=" + loggedIn.email, {
       method: "GET",
@@ -17,7 +18,7 @@ const Order = () => {
       .then((response) => response.json())
       .then((data) => setOrders(data));
   }, []);
-
+  const totalPrice = orders.reduce((total, order) => total + parseInt(order.productPrice), 0)
   return (
     <div>
       <Header></Header>
@@ -25,18 +26,24 @@ const Order = () => {
         className="tableContainer"
         style={{ backgroundColor: "white", borderRadius: "20px" }}
       >
+        <div className="d-flex justify-content-between">
         <h3 className="mt-5">You have {orders.length} Ordered Product</h3>
+        <h3 className="mt-5" style={{color: 'red'}}>Total Spent: {totalPrice}</h3>
+        </div>
+        
         <Table responsive borderless className=" mt-3 table">
           <thead style={{ backgroundColor: "#F5F6FA" }}>
             <tr>
+              <th>Serial</th>
               <th>Order Date</th>
               <th>Product Name</th>
               <th>Price</th>
             </tr>
           </thead>
           <tbody>
-            {orders.map((order) => (
+            {orders.map((order,index) => (
               <tr>
+                <td>{index+1}</td>
                 <td>{order.orderDate}</td>
                 <td>{order.productName}</td>
                 <td>${order.productPrice}</td>
